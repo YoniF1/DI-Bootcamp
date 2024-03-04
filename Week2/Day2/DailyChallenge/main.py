783ae51087f1734d48e2b302589176dc8a23722d
Yonatan <yonatanfilar@gmail.com>
2024-03-04 16:37:46 +0200

    Change code to account for non consecutive letter

--- Week2/Day2/DailyChallenge/main.py
+++ Week2/Day2/DailyChallenge/main.py
@@ -17,15 +17,12 @@
 
 word = input("Enter a word with duplicate letters: ")
 
-dict = {}
+final_list = []
 
-word_list = list(word)
-
-for i in word_list:
-    if i in dict:
-        continue
-    else:
-        dict[i] = 1
-
-list_of_keys = list(dict.keys())
-print(''.join(list_of_keys))
+i=0
+while i < len(word):
+    if i == len(word) -1 or word[i] != word[i + 1]:
+        final_list.append(word[i])
+    i+=1
+    
+print(''.join(final_list))
