e240c8c7191807d72179074291015313ae93c07d
Yonatan <yonatanfilar@gmail.com>
2024-03-04 10:11:35 +0200

    Range end is non-inclusive

--- Week2/Day1/ExercisesXPGold/main.py
+++ Week2/Day1/ExercisesXPGold/main.py
@@ -6,11 +6,11 @@ print("Hello world\n" *4 + "I love python\n" * 4)
 
 month = int(input("Choose a month from 1-12: "))
 
-if month in range(3, 5):
+if month in range(3, 6):
     print("Spring")
-elif month in range(6, 8):
+elif month in range(6, 9):
     print("Summer")
-elif month in range(9, 11):
+elif month in range(9, 12):
     print("Autumn")
 else:
     print("Winter")
