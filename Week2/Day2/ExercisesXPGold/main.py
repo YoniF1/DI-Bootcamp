23e44ce3553bf62358761a670ecebd2fb3bc0f2c
Yonatan <yonatanfilar@gmail.com>
2024-03-05 23:37:57 +0200

    Half complete Day2 exercisesXPGold

---
+++ Week2/Day2/ExercisesXPGold/main.py
@@ -0,0 +1,42 @@
+#Exercise1
+
+# first_list = [1, 2, 3, 4, 5]
+# second_list = [6, 7, 8]
+
+# first_list.extend(second_list)
+
+# print(first_list)
+
+
+#Exercise2
+
+# i=0
+# for i in range(1500, 2501):
+#     if i % 5 == 0:
+#         print(i)
+#     elif i % 7 == 0:
+#         print(i)
+#     else:
+#         continue
+
+# Exercise3
+
+# names = ['Samus', 'Cortana', 'V', 'Link', 'Mario', 'Cortana', 'Samus']
+
+# user_input = input("Give me a name: ")
+
+# index = names.index(user_input) if user_input in names else -1
+
+# if index >= 0:
+#     print(index)
+# else:
+#     print("The name is not in the list")
+
+# Exercise4
+    
+for i in [1, 2, 3]:
+    user_input = int(input())
+
+
+
+
